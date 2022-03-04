63644b07c2a232a003837dc3f2064b6998b83520
Brandyn Britton <Brandynbb96@gmail.com>
2022-03-04 23:27:30 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const messages = {
+  "intro": {
+    "en": "Hello Selene",
+    "es": "Hola Selene"
+  },
+  "second-step": {
+    "en": "You told me to be creative and I could have done something cheesy like write you a message in the sky or send you a pamper but I want to make you something useful as well. Something we could build on.",
+    "es": "Me dijiste que fuera creativo y podría haber hecho algo cursi como escribirte un mensaje en el cielo o enviarte un mimo, pero también quiero hacerte algo útil. Algo sobre lo que podamos construir."
+  }
+
+}
+
 function App() {
+
+
+  const [language, setLanguage] = useState("es");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id="introduction" className="fullscreen">
+        <div className="center">
+          <span className="fancy-font">
+            {messages["intro"][language]}
+          </span>
+        </div>
+      </div>
+
+      <div id="step-two" className="fullscreen">
+        <div className="center">
+          <span className="fancy-font">
+            {messages["second-step"][language]}
+          </span>
+        </div>
+      </div>
+
+    <div id="language-selector">
+      <span onClick={() => {setLanguage("en")}}>EN</span>|
+      <span onClick={() => {setLanguage("es")}}>ES</span>
     </div>
+
+    </div>
+
   );
 }
 
