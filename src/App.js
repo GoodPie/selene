1ee8410be6df1603375b326ca9f3bd6096fc7fa4
Brandyn <brandynbb96@gmail.com>
2022-03-08 13:37:19 +0800

    update: changed step two message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
-
-const messages = {
-  "intro": {
-    "en": "Hello Selene",
-    "es": "Hola Selene"
-  },
-  "second-step": {
-    "en": "You told me to be creative and I could have done something cheesy like write you a message in the sky or send you a pamper but I want to make you something useful as well. Something we could build on.",
-    "es": "Me dijiste que fuera creativo y podría haber hecho algo cursi como escribirte un mensaje en el cielo o enviarte un mimo, pero también quiero hacerte algo útil. Algo sobre lo que podamos construir."
-  }
-
-}
+import { messages } from './messages';
 
 function App() {
 
---
+++ src/messages.js
@@ -0,0 +1,10 @@
+export const messages = {
+  "intro": {
+    "en": "Hello Selene",
+    "es": "Hola Selene"
+  },
+  "second-step": {
+    "en": "You told me to be creative. I wanted to make this a massive project but also wanted to get this out to you as soon as I could. So I made this website. It's a little bit of a mess but it's a start. I hope you enjoy it and I hope we can keep adding to it.",
+    "es": "Me dijiste que fuera creativo. Quería hacer de este un proyecto masivo, pero también quería hacértelo saber lo antes posible. Así que hice este sitio web. Es un poco complicado, pero es un comienzo. Espero que lo disfruten y que podamos seguir agregando más."
+  }
+};
